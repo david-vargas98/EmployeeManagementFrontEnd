50a7bc621dcf0d157b6a34db34546044de16024f
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 16:55:32 -0600

    feat: EmployeeTable into app.
    Inluding EmployeeTable component into the main component 'app'.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeTable } from "./employee-table/employee-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeTable],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

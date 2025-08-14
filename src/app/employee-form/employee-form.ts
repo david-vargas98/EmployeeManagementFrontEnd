c843ec07ad40cd340aab356cd12e3b487e3cfdf1
david-vargas98 <edgar981125@live.com.mx>
2025-08-14 14:38:39 -0600

    feat: MessageError | next and error callbacks.
    A message was added to be displayed if an error occurs.
    
    Two callbacks were added for the result of the subscriber, if succeded it returns to root page, if not, an error message is assigned to a variable.

--- src/app/employee-form/employee-form.ts
+++ src/app/employee-form/employee-form.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee-service';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css'
 })
@@ -20,10 +22,20 @@ export class EmployeeForm {
     position: ''
   }
 
-  constructor(private employeeService: EmployeeService){}
+  errorMessage: string = "";
+
+  constructor(private employeeService: EmployeeService, private router: Router){}
 
   onSubmit(): void{
     this.employeeService.createEmployee(this.employee)
-    .subscribe((result) => console.log(result));
+    .subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.log(err);
+        this.errorMessage = `Error: ${err.status} - ${err.message}`;
+      }
+    });
   }
 }

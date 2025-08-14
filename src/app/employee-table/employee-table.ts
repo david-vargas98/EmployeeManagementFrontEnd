a87d06e53f1ed1a1c669ce49ef30dfe41f9a3599
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 23:07:02 -0600

    fix: NgFor import and html table.
    Making NgFor import and adding table to the html template.

--- src/app/employee-table/employee-table.ts
+++ src/app/employee-table/employee-table.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee-service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'employee-table',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './employee-table.html',
   styleUrl: './employee-table.css'
 })

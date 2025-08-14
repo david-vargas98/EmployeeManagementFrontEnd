8265dd7addedc6b9249bbbaeac6f56e4f86ddedc
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 22:31:08 -0600

    feat: EmployeeTable implementation.
    Pertienent imports such as 'OnInit', 'EmployeeService' were brought to the component.
    
    The constructor was created and EmployeeService was injected in it, the class now implements OnInit which is the hook where the logic to get the list of employees was implemented.

--- src/app/employee-table/employee-table.ts
+++ src/app/employee-table/employee-table.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
+import { EmployeeService } from '../employee-service';
 
 @Component({
   selector: 'employee-table',
-  imports: [EmployeeTable],
+  imports: [],
   templateUrl: './employee-table.html',
   styleUrl: './employee-table.css'
 })
-export class EmployeeTable {
+export class EmployeeTable implements OnInit {
 
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(data);
+    });
+  }
 }

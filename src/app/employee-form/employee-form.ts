9297ad44b041ff76ff33d23301c760a1ed88b529
david-vargas98 <edgar981125@live.com.mx>
2025-08-14 01:38:33 -0600

    feat: Employee property and onSubmit().
    The employee object for data binding were created and the mock of onSubmit() function as well.

--- src/app/employee-form/employee-form.ts
+++ src/app/employee-form/employee-form.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css'
 })
 export class EmployeeForm {
 
+  employee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    position: ''
+  }
+
+  onSubmit(): void{
+    console.log(this.employee);
+  }
 }

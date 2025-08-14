f4a58b851c217697482bd436ce62ba310d1bf1e3
david-vargas98 <edgar981125@live.com.mx>
2025-08-14 00:52:08 -0600

    feat: Navbar.
    Adding the navbar element and its pertinent module.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { EmployeeTable } from "./employee-table/employee-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeTable],
+  imports: [RouterOutlet, EmployeeTable, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
